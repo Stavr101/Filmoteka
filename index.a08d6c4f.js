aafccc3340dc8e8cca5748e05c12e39d25b9e999
MaxsaVika <96749210+MaxsaVika@users.noreply.github.com>
2022-08-24 12:08:21 +0000

    Deploying to gh-pages from @ Stavr101/Filmoteka@3dad6a16c808ac44ceb8579e44e9044b7fb99fb0 🚀

---
+++ index.a08d6c4f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".sidebar");window.addEventListener("resize",(function(){window.innerWidth>1090?e.classList.remove("collapse"):e.classList.add("collapse")}))}();
+//# sourceMappingURL=index.a08d6c4f.js.map
---
+++ index.fb46ba0e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".sidebar");window.addEventListener("resize",(function(){window.innerWidth>1090?e.classList.remove("collapse"):e.classList.add("collapse")}));
+//# sourceMappingURL=index.fb46ba0e.js.map
