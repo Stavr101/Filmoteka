db84c4d510183d68e6da8112aa14e2032c1cb18e
Stavr101 <95885361+Stavr101@users.noreply.github.com>
2022-08-25 13:43:33 +0000

    Deploying to gh-pages from @ Stavr101/Filmoteka@0bb6e00cf508f375526b64a48eb41b590ebabdbe 🚀

--- index.19de3c6b.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".sidebar");window.addEventListener("resize",(function(){window.innerWidth>1090?e.classList.remove("collapse"):e.classList.add("collapse")}));
-//# sourceMappingURL=index.19de3c6b.js.map
--- index.2e26bbeb.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".sidebar");window.addEventListener("resize",(function(){window.innerWidth>1090?e.classList.remove("collapse"):e.classList.add("collapse")}))}();
-//# sourceMappingURL=index.2e26bbeb.js.map
